2655238c1969ff286a4b0fef894986f80af38e4b
kavya23280 <kavya23280@iiitd.ac.in>
2024-10-22 05:13:38 +0530

    music

--- core/src/main/java/com/test_game/main/Core.java
+++ core/src/main/java/com/test_game/main/Core.java
@@ -1,25 +1,63 @@
 package com.test_game.main;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.test_game.main.HomeScreen;
 
 public class Core extends Game {
     public SpriteBatch batch;
+    private Music bgMusic;
+    private boolean isMusicOn;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+
+        // Initialize background music
+        bgMusic = Gdx.audio.newMusic(Gdx.files.internal("background_music.mp3"));
+        bgMusic.setLooping(true); // Make the music loop
+        isMusicOn = true; // Default state of music is ON
+        playMusic(); // Start playing the music when the game starts
+
+        // Set the initial screen (e.g., HomeScreen)
         setScreen(new HomeScreen());
     }
 
     @Override
     public void render() {
-        super.render();
+        super.render(); // Render the current screen
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+        bgMusic.dispose(); // Dispose of the music when done
+    }
+
+    // Music control methods
+    public void playMusic() {
+        if (!bgMusic.isPlaying()) {
+            bgMusic.play(); // Play the music if it's not already playing
+        }
+    }
+
+    public void pauseMusic() {
+        if (bgMusic.isPlaying()) {
+            bgMusic.pause(); // Pause the music if it's currently playing
+        }
+    }
+
+    public void toggleMusic() {
+        if (isMusicOn) {
+            pauseMusic(); // Pause the music if it's currently playing
+        } else {
+            playMusic(); // Play the music if it's not playing
+        }
+        isMusicOn = !isMusicOn; // Toggle the music state
+    }
+
+    public boolean isMusicOn() {
+        return isMusicOn; // Return the current state of the music
     }
 }
--- core/src/main/java/com/test_game/main/HomeScreen.java
+++ core/src/main/java/com/test_game/main/HomeScreen.java
@@ -2,6 +2,7 @@ package com.test_game.main;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
